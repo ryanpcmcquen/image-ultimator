d3ff6688177e0a921ce866b01404270d9da9e3d5
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-10-16 14:18:36 -0700

    create backup option

--- optomatic-a-fy.sh
+++ optomatic-a-fy.sh
@@ -2,6 +2,22 @@
 
 ## mozjpeg, jpegoptim and optipng must all be installed
 
+read -r -p "Would you like to BACKUP the original files? [y/N]: " response
+case $response in
+  [yY][eE][sS]|[yY])
+    export BACKUPIMAGES=true;
+    echo "You are backing things up, better safe than sorry!";
+    ;;
+  *)
+    echo "You are a risk taker.";
+    ;;
+esac
+
+if [ $BACKUPIMAGES = true ]; then
+  mkdir -p ./original-image-files/
+  find . -name "*.jpg" -type f -exec cp {} ./original-image-files/{} \;
+  find . -name "*.png" -type f -exec cp {} ./original-image-files/{} \;
+fi
 
 if [ "$( uname -s )" = Darwin ]; then
   ## mac version
