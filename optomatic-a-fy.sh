fb71e051d84058f582aa63e8c80866885cfd22aa
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-10-16 13:35:50 -0700

    can't use some options on mac os

--- optomatic-a-fy.sh
+++ optomatic-a-fy.sh
@@ -5,8 +5,8 @@
 
 if [ "$( uname -s )" = Darwin ]; then
   ## mac version
-  ls --color=never *.jpg | xargs -i /usr/local/Cellar/mozjpeg/2.1/bin/jpegtran {} > % %.old
-  ls --color=never *.jpg | xargs -i jpegoptim -m65 {}
+  ls *.jpg | xargs /usr/local/Cellar/mozjpeg/2.1/bin/jpegtran {} > new.%
+  ls *.jpg | xargs jpegoptim -m65 {}
 else
   ## add linux version here
   echo "coming soon"
