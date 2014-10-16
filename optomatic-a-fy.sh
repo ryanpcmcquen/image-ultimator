7eca6d4d08685c154944dcb7d21219b9c10d3d9b
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-10-16 13:28:22 -0700

    Create optomatic-a-fy.sh

---
+++ optomatic-a-fy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## mozjpeg, jpegoptim and optipng must all be installed
+
+
+if [ "$( uname -s )" = Darwin ]; then
+  ## mac version
+  ls --color=never *.jpg | xargs -i /usr/local/Cellar/mozjpeg/2.1/bin/jpegtran {} > % %.old
+  ls --color=never *.jpg | xargs -i jpegoptim -m65 {}
+else
+  ## add linux version here
+fi
